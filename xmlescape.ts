9ecf127fc4c72c3767c61cdf31beadc26fb62e77
Pascal Esemann <pascal.esemann@gusarin.de>
2018-03-05 13:17:08 +0100

    fix Color-Parsing Issue

--- xmlescape.ts
+++ xmlescape.ts
@@ -15,17 +15,17 @@ export function escapeXml(str: string, ignore: any) {
     ignore = (ignore || '').replace(/[^&"<>\']/g, '');
     pattern = '([&"<>\'])'.replace(new RegExp('[' + ignore + ']', 'g'), '');
    
-
-    str = str.replace(/\u001b/g, "");
-    str = str.replace(/\u005b/g, "");
-    str = str.replace(/\u0032/g, "");
-    str = str.replace(/\u006d/g, "");
-
-
-
-
-    
-
+    //Remove Colloring from Results, to avoid failures in Allure
+    if(!(str.toString().startsWith('    at'))){
+        str = str.replace(/\u001b/g, "");
+        str = str.replace(/\u005b/g, "");
+        str = str.replace(/\u0032/g, "");
+        str = str.replace(/\u006d/g, "");
+        
+        str = str.replace(/\u0031/g, "");
+        str = str.replace(/\u0039/g, "");
+        str = str.replace(/\u0033/g, "");  
+    } 
 
 
     return str.replace(new RegExp(pattern, 'g'), function (str, item) {
