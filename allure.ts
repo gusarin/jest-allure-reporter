6779b87692777bf36b62d7ea8d1ccb28737e7335
Pascal Esemann <pascal.esemann@gusarin.de>
2018-03-06 13:43:54 +0100

    Add string template for XML-Creation

--- allure.ts
+++ allure.ts
@@ -10,40 +10,41 @@ import { Testcase } from "./testcase";
 import save = require("save-file");
 import allure = require("allure-commandline");
 // FIXME imports
-import fs = require('fs-extra');
+import fse = require('fs-extra');
 import rp = require('rootpath');
 import { escapeXml } from "./xmlescape";
 import strip_ansi = require("strip-ansi");
+import * as fs from "fs";
+import * as st from "string-template";
 
 
 export class Allure {
     // TODO solve via template
     //Generating an String containing the test-results in an XML-Format which is readable for Allure, and calling a method writing the string to a file. 
     static generateAllureXMLOutput(testsuite: Testsuite) {
-        var allureXMLString = "";
-        allureXMLString += "<?xml version='1.0'?>\n";
-        allureXMLString += "<ns2:test-suite xmlns:ns2='urn:model.allure.qatools.yandex.ru' start='" + testsuite.startTime + "' stop='" + testsuite.stopTime + "'>\n";
-        allureXMLString += "<name>" + testsuite.name + "</name>\n";
-        allureXMLString += "<title>" + testsuite.name + "</title>\n";
-        allureXMLString += "<test-cases>\n";
-        testsuite.testcases.forEach((testcase: Testcase) => {
-            allureXMLString += "<test-case start='" + testcase.startTime + "' status='" + testcase.status + "' stop='" + testcase.stopTime + "'>\n";
-            allureXMLString += "<name>" + testcase.fullName + "</name>\n";
-            allureXMLString += "<title>" + testcase.title + "</title>\n";
-            allureXMLString += "<failure>\n";
-            const messages = testcase.failureMessages ? testcase.failureMessages.split("\n").map((line) => {
-                var esc: string = strip_ansi(line);
-                esc = escapeXml(esc, '');
-                return esc;
-            }) : [];
 
-            allureXMLString += "<message>" + messages.join("\n") + "</message>\n";
-            allureXMLString += "<stack-trace>" + messages.join("\n") + "</stack-trace>\n";
-            allureXMLString += "</failure>\n";
-            allureXMLString += "</test-case>\n";
+        var xmlSuiteTemplate: string = fs.readFileSync("../../../jest-allure-reporter/xmlSuiteTemplate","utf8").toString();
+        var xmlTestcaseTemplate: string = fs.readFileSync("../../../jest-allure-reporter/xmlTestcaseTemplate","utf8").toString();
+        var testcases: string ="";
+        
+        testsuite.testcases.forEach((testcase: Testcase) => {
+            testcases += st(xmlTestcaseTemplate,{
+                testStartTime: testcase.startTime
+                ,testStatus: testcase.status
+                ,testStopTime: testcase.stopTime
+                ,testName: testcase.fullName
+                ,testTitle: testcase.title
+                ,testMessage: escapeXml(strip_ansi(testcase.failureMessages), '')
+            });
         });
-        allureXMLString += "</test-cases>\n";
-        allureXMLString += "</ns2:test-suite>\n";
+
+        var allureXMLString: string = st(xmlSuiteTemplate,{
+            suiteStartTime: testsuite.startTime
+            ,suiteStopTime: testsuite.stopTime
+            ,suiteName: testsuite.name
+            ,testCases: testcases
+        });
+
         if (!(testsuite.name.includes('undefined'))) {
             this.writeXMLToFile(allureXMLString, testsuite.name);
         }
@@ -52,7 +53,7 @@ export class Allure {
     //removeOldResults from Filesystem
     static removeOldResults() {
         //remove old results
-        fs.remove('/tmp/allure-results/*', (err) => {
+        fse.remove('/tmp/allure-results/*', (err) => {
             if (err) console.log("Deleting old result-files failed. There might be no old results or something unexpected happened.");
             else console.log('Successfully deleted old results!');
         });
@@ -68,7 +69,7 @@ export class Allure {
     static generateReport() {
         //get history
         rp();
-        fs.copy('allure-report/history/', '/tmp/allure-results/history/', err => {
+        fse.copy('allure-report/history/', '/tmp/allure-results/history/', err => {
             if (err) return console.log("Copying old history failed. There might be no history or something unexpected happened.")
             else console.log('Successfully copied history!')
         })
