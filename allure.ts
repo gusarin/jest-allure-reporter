1b1218bfbde114b1f24b3fa598b86832e5f25912
Hannes Römer <hannes.roemer@telekom.de>
2018-04-16 13:50:02 +0200

    #1 fixed temp directory handling

--- allure.ts
+++ allure.ts
@@ -11,11 +11,13 @@ import save = require("save-file");
 import allure = require("allure-commandline");
 import fse = require('fs-extra');
 import rp = require('rootpath');
-import * as path from "path";
 import { escapeXml } from "./xmlescape";
 import * as fs from "fs";
 import * as st from "string-template";
+import * as path from "path";
+import * as os from "os";
 
+const tempDir = path.resolve(path.join(os.tmpdir(), `./allure-results33`));
 
 export class Allure {
     //Generating an String containing the test-results in an XML-Format which is readable for Allure, and calling a method writing the string to a file. 
@@ -23,18 +25,18 @@ export class Allure {
 
         const xmlSuiteTempPath = path.resolve(path.join(__dirname, "./xmlSuiteTemplate"));
         const xmlTestCasePath = path.resolve(path.join(__dirname, "./xmlTestcaseTemplate"));
-        const xmlSuiteTemplate: string = fs.readFileSync(xmlSuiteTempPath,"utf8").toString();
-        const xmlTestcaseTemplate: string = fs.readFileSync(xmlTestCasePath,"utf8").toString();
+        const xmlSuiteTemplate: string = fs.readFileSync(xmlSuiteTempPath, "utf8").toString();
+        const xmlTestcaseTemplate: string = fs.readFileSync(xmlTestCasePath, "utf8").toString();
         let testcases: string = "";
 
         testsuite.testcases.forEach((testcase: Testcase) => {
-            testcases += st(xmlTestcaseTemplate,{
+            testcases += st(xmlTestcaseTemplate, {
                 testStartTime: testcase.startTime
-                ,testStatus: testcase.status
-                ,testStopTime: testcase.stopTime
-                ,testName: testcase.fullName
-                ,testTitle: testcase.title
-                ,testMessage: escapeXml(testcase.failureMessages, '')
+                , testStatus: testcase.status
+                , testStopTime: testcase.stopTime
+                , testName: testcase.fullName
+                , testTitle: testcase.title
+                , testMessage: escapeXml(testcase.failureMessages, '')
             });
         });
 
@@ -53,7 +55,7 @@ export class Allure {
     //removeOldResults from Filesystem
     static removeOldResults() {
         //remove old results
-        fse.remove('/tmp/allure-results/*', (err) => {
+        fse.remove(`${tempDir}/*`, (err) => {
             if (err) console.log("Deleting old result-files failed. There might be no old results or something unexpected happened.");
             else console.log('Successfully deleted old results!');
         });
@@ -62,20 +64,22 @@ export class Allure {
 
     //Write the given String to file, as an input for the report generation
     static writeXMLToFile(xmlString: String, name: String) {
-        save(xmlString, '/tmp/allure-results/' + name + '-testsuite.xml');
+        const target = path.resolve(path.join(tempDir, `./${name}-testsuite.xml`));
+        save(xmlString, target);
     }
 
     //Generate the Allure-report.
     static generateReport() {
         //get history
         rp();
-        fse.copy('allure-report/history/', '/tmp/allure-results/history/', err => {
+        const historyDir = path.resolve(path.join(tempDir, "./history/"));
+        fse.copy('allure-report/history/', historyDir, err => {
             if (err) return console.log("Copying old history failed. There might be no history or something unexpected happened.")
             else console.log('Successfully copied history!')
         });
 
         // returns ChildProcess instance
-        const generation = allure(['generate', '/tmp/allure-results', '--clean']);
+        const generation = allure(['generate', `${tempDir}`, '--clean']);
 
         generation.on('exit', function (exitCode: any) {
             console.log('Generation is finished with code:', exitCode);
